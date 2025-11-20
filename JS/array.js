feb5b0d127e213efa6ee5dae102679385783240d
Disha Sikder <sikderdisha715@gmail.com>
2025-11-21 00:35:21 +0600

    Add files via upload

---
+++ JS/array.js
@@ -0,0 +1,12 @@
+//array
+var arry = [1,2,3,4,5,6,7,8,9];
+console.log(arry); //print array
+console.log(arry[3]); //print the value where index is 3
+
+//set new value
+arry[9]=24;
+console.log(arry);
+
+//change any number
+arry[0]='disha';
+console.log(arry);
