e1176d68ef0ccc60759dd2eedd5b579b3f048368
Disha Sikder <sikderdisha715@gmail.com>
2025-11-21 00:24:39 +0600

    Add files via upload

---
+++ JS/Predefined_functions.js
@@ -0,0 +1,27 @@
+//math.abs
+let num1=5;
+let num2=15;
+let sub=num1-num2;
+console.log(Math.abs(sub))
+
+//math.ceil
+num1=3.878;
+num2=4.4;
+
+console.log(Math.round(num1))
+console.log(Math.ceil(num2))
+
+//math.floor
+console.log(Math.floor(num2))
+
+//math.random
+console.log(Math.random())
+
+let lotary;
+console.log(Math.random(lotary))
+
+lotary=Math.random() * 100;
+console.log(lotary);
+
+lotary=Math.random() + 10;
+console.log(lotary);
