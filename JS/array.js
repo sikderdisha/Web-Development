5c94fb2a9392a80b36651c2f4f52bfca47440d3a
Disha Sikder <sikderdisha715@gmail.com>
2025-11-22 01:29:19 +0600

    Add files via upload

--- JS/array.js
+++ JS/array.js
@@ -10,3 +10,32 @@ console.log(arry);
 //change any number
 arry[0]='disha';
 console.log(arry);
+
+//push()>>insert new value in the end of the array
+arry.push('mim');
+console.log(arry)
+
+//pop()>>remove the last element from array
+arry.pop();
+console.log(arry)
+
+//shift method>>delete the 1st elememnt 
+arry.shift();
+console.log(arry)
+
+//unshift() >>add element in index 0
+arry.unshift('antora');
+console.log(arry)
+
+//slice()
+var newarry=arry.slice(0,5);
+console.log(newarry)
+
+//splice()
+//array.splice(start, deleteCount, item1, item2, ...);
+var mydata=['disha','puja','chaity','antora','anamika','tanishka','mukty','amal','prodip','mariam'];
+console.log(mydata);
+mydata.splice(1,6); //remove data 1 to 6
+console.log('new data will be: ' +mydata);
+mydata.splice(4,0,'munia','dipali'); //last a data add korbe,kisu delete hbe na cause of index 4
+console.log(mydata)
