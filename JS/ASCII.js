8a2e22cc9e6d78f25304783674060d2f75746d0a
Disha Sikder <sikderdisha715@gmail.com>
2025-11-25 04:01:52 +0600

    Add files via upload

---
+++ JS/ASCII.js
@@ -0,0 +1,16 @@
+//charCodeAt(index)
+//return the ASCII of a string
+
+let text='ABCDE';
+console.log(text.charCodeAt(0));
+
+// fromCharCode()
+let string='AB';
+console.log(string.fromCharCode(0))
+
+
+// print next letter 
+let string2 = 'A';
+let nextLetter = String.fromCharCode(letter.charCodeAt(0) + 1);
+
+console.log(nextLetter);
---
+++ JS/nestedObject.js
@@ -0,0 +1,29 @@
+let person = {
+    name: 'abdul',
+    age: 29,
+    address: {
+        present: '122/23, mugda',
+        permanent: 'azimpur'
+    },
+    contact: {
+        work: '01918287274',
+        home: '0903924925'
+    }
+};
+
+console.log(person);
+console.log(person.address.present);
+console.log(person.contact);
+
+
+
+let myData={
+    name:'disha',
+    address:[
+        {houseNo:122/23 , street:'mugda' ,division:'dhaka'},
+         {houseNo:12/23 , street:'khilga' ,division:'saver'},
+          {houseNo:22/23 , street:'mugda' ,division:'KHulna'}
+    ]
+};
+console.log(myData);
+console.log(myData.address[2].division);
