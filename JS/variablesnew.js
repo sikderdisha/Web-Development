1a91c836155c95877c73438648186e693fb5592b
Disha Sikder <sikderdisha715@gmail.com>
2025-11-30 23:28:14 +0600

    Add files via upload

---
+++ JS/variablesnew.js
@@ -0,0 +1,16 @@
+//let
+let num='disha';
+console.log(num);
+
+//re-assign possible
+num='puja';
+console.log(num);
+
+//re-declare not possible
+//let num='mim';
+
+//var
+var mim='disha';
+console.log(mim);
+var mim='mim';  //re-declare 
+console.log(mim);
