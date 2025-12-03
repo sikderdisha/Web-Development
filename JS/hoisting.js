a438bf20f4e30c79ae72197609a3935105da3772
Disha Sikder <sikderdisha715@gmail.com>
2025-12-04 00:54:46 +0600

    Add files via upload

---
+++ JS/hoisting.js
@@ -0,0 +1,17 @@
+//declare
+var name='hamid';
+console.log(name);
+
+name='disha'; //assign value
+console.log(name);
+
+//assign value before declaration but still it works
+x=5;
+console.log(x);
+var x;
+
+
+//but when we use let here we get error
+// y=5;
+// console.log(y);
+// let y;
